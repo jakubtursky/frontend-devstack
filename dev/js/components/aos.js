dfe264e4caaee426ba49a5ec3d05d44f7a1517de
jakubtursky <jakubtursky769@gmail.com>
2020-05-22 17:59:37 +0200

    Clear repositary

--- dev/js/components/aos.js
+++
@@ -1,7 +0,0 @@
-//====================================================
-//  Initialization Aos
-//  (data-aos="fade-in" data-aos-delay="300" data-aos-once="true" data-aos-duration="700")
-//====================================================
-function fn_components_aos(){
-	AOS.init();
-}
